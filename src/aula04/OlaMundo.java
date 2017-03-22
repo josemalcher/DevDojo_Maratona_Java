589357db088e3bfadbc8a1add8042b35394df7ec
José Malcher Junior <contato@josemalcher.net>
2017-03-22 10:33:07 -0300

    Aula 04: inicial - ajustes

--- src/aula04/OlaMundo.java
+++ src/aula04/OlaMundo.java
@@ -2,14 +2,14 @@ package aula04;
 
 /**
  * Created by josemalcher on 14/03/2017.
- *
+ * <p>
  * DevDojo
  * Curso Java Completo - Aula 04: Download do ambiente de desenvolvimento
  * https://www.youtube.com/watch?v=blYNhqo7yhk
- *
  */
 public class OlaMundo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("OLá Mundo - DEVDOJO");
+
     }
 }
