3f08b1c8901c7d42842366d06defcca850494ffc
José Malcher Junior <contato@josemalcher.net>
2017-03-21 22:11:00 -0300

    Aula 18: Arrays

---
+++ src/aula18Arrays/Arrays1.java
@@ -0,0 +1,20 @@
+package aula18Arrays;
+
+/**
+ * Created by josemalcher on 21/03/2017.
+ */
+public class Arrays1 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+
+        idades[0] = 10;
+        idades[1] = 20;
+        idades[2] = 30;
+
+        System.out.println("Idade é 1: " + idades[0]);
+        System.out.println("Idade é 2: " + idades[1]);
+        System.out.println("Idade é 3: " + idades[2]);
+
+
+    }
+}
