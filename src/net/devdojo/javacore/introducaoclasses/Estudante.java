b87f5d2dac65c5fcb86b6999f984350cddd36305
José Malcher Junior <contato@josemalcher.net>
2017-03-22 16:19:39 -0300

    Aula 24: Introdução a classes parte 1

---
+++ src/net/devdojo/javacore/introducaoclasses/Estudante.java
@@ -0,0 +1,16 @@
+package net.devdojo.javacore.introducaoclasses;
+
+/**
+ * Criado por José Stélio Malcher Junior em 22/03/2017.
+ * www.josemalcher.net
+ *
+ * Curso Java Completo - Aula 24: Introdução a classes parte 1
+ * https://www.youtube.com/watch?v=Godi9y-B2aI&index=25&list=PL62G310vn6nHrMr1tFLNOYP_c73m6nAzL
+ *
+ */
+public class Estudante {
+    public String nome;
+    public String matricula;
+    public int idade;
+
+}
