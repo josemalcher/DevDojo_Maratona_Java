28c7dd8a7aae7c344fbcdacf7cbedaa29115a2cc
José Malcher Junior <contato@josemalcher.net>
2017-03-22 17:42:06 -0300

    Aula 27: Introdução a métodos

---
+++ src/net/devdojo/javacore/introducaometodos/classes/Calculadora.java
@@ -0,0 +1,19 @@
+package net.devdojo.javacore.introducaometodos.classes;
+
+/**
+ * Criado por José Stélio Malcher Junior em 22/03/2017.
+ * www.josemalcher.net
+ *
+ * DevDojo
+ * Curso Java Completo - Aula 27: Introdução a métodos
+ * https://www.youtube.com/watch?v=GEjFD9qvUVM
+ *
+ */
+public class Calculadora {
+    public void somaDoisNumero(){
+        System.out.println(5+5);
+    }
+    public void subtraiDoisNumeros(){
+        System.out.println(10-8);
+    }
+}
---
+++ src/net/devdojo/javacore/introducaometodos/test/CalculadoraTest.java
@@ -0,0 +1,22 @@
+package net.devdojo.javacore.introducaometodos.test;
+
+import net.devdojo.javacore.introducaometodos.classes.Calculadora;
+
+/**
+ * Criado por José Stélio Malcher Junior em 22/03/2017.
+ * www.josemalcher.net
+ *
+ * DevDojo
+ * Curso Java Completo - Aula 27: Introdução a métodos
+ * https://www.youtube.com/watch?v=GEjFD9qvUVM
+ *
+ */
+public class CalculadoraTest {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+
+        calc.somaDoisNumero();
+        System.out.println("---");
+        calc.subtraiDoisNumeros();
+    }
+}
