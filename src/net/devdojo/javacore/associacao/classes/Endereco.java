4a55f571b194f8864bcbf1f9692873aca24bb998
José Malcher Junior <contato@josemalcher.net>
2017-03-27 07:16:38 -0300

    Aula 45: UML e Associação de classes

---
+++ src/net/devdojo/javacore/associacao/classes/Endereco.java
@@ -0,0 +1,19 @@
+package net.devdojo.javacore.associacao.classes;
+
+/**
+ * Criado por José Stélio Malcher Jr. on 27/03/2017.
+
+ * DevDojo
+ * Curso Java Completo - Aula 45: UML e Associação de classes
+ * https://www.youtube.com/watch?v=3OR-8ExlwvA&index=46&list=PL62G310vn6nHrMr1tFLNOYP_c73m6nAzL
+ *
+ */
+public class Endereco {
+
+    private String rua;
+    private String bairro;
+    private String estado;
+
+    //Não vincular...
+    //private Pessoa pessoas[];
+}
---
+++ src/net/devdojo/javacore/associacao/classes/Pessoa.java
@@ -0,0 +1,14 @@
+package net.devdojo.javacore.associacao.classes;
+
+/**
+ * Criado por José Stélio Malcher Jr. on 27/03/2017.
+ *
+ * DevDojo
+ * Curso Java Completo - Aula 45: UML e Associação de classes
+ * https://www.youtube.com/watch?v=3OR-8ExlwvA&index=46&list=PL62G310vn6nHrMr1tFLNOYP_c73m6nAzL
+ *
+ */
+public class Pessoa {
+    private Departamento departamento;
+    private Endereco endereco;
+}
