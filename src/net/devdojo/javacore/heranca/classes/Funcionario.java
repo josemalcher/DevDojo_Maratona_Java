14c2b1eeb97724aeb4cc6b8e7cfaf52160dfa7b2
José Malcher Junior <contato@josemalcher.net>
2017-03-28 23:27:29 -0300

    Aula 51: Herança parte 2 e referência super

--- src/net/devdojo/javacore/heranca/classes/Funcionario.java
+++ src/net/devdojo/javacore/heranca/classes/Funcionario.java
@@ -11,6 +11,11 @@ public class Funcionario extends Pessoa {
 
     private double salario;
 
+    public void imprime(){
+        super.imprime();
+        System.out.println("Salário: "+this.salario);
+    }
+
     public double getSalario() {
         return salario;
     }
