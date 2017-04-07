9729f9f38d05e763620f03a2694e08d07faf6c03
José Malcher Junior <contato@josemalcher.net>
2017-04-07 14:39:00 -0300

    Aula 63: Interfaces pt 02

--- src/net/devdojo/javacore/interfaces/classes/Carro.java
+++ src/net/devdojo/javacore/interfaces/classes/Carro.java
@@ -4,9 +4,11 @@ package net.devdojo.javacore.interfaces.classes;
  * Criado por José Stélio Malcher Jr. on 07/04/2017.
  */
 public class Carro implements Tributavel {
+    // private > default > protected > public
+
 
     @Override
     public void calcularImposto() {
-
+    //sempre deverá ser public!
     }
 }
