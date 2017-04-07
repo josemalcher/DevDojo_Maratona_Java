dad018339a43b3e1d69f64a7171bab68383a3c44
José Malcher Junior <contato@josemalcher.net>
2017-04-07 18:11:11 -0300

    Aula 68: Exceptions pt 02

---
+++ src/net/devdojo/javacore/exception/runtimeexception/RuntimeexceptionTest.java
@@ -0,0 +1,30 @@
+package net.devdojo.javacore.exception.runtimeexception;
+
+/**
+ * Criado por José Stélio Malcher Jr. on 07/04/2017.
+ * <p>
+ * <p>
+ * Curso Java Completo - Aula 68: Exceptions pt 02
+ * https://www.youtube.com/watch?v=eZCGf_lTx0E&list=PL62G310vn6nHrMr1tFLNOYP_c73m6nAzL&index=69
+ */
+public class RuntimeexceptionTest {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 0;
+        if (b != 0) {
+            int c = a / b;
+            System.out.println(a);
+        }
+
+        //Object o = null;
+        //System.out.println(o.toString());
+        //java.lang.NullPointerException
+
+        int[] ar = new int[2];
+        System.out.println(ar[2]);
+        //java.lang.ArrayIndexOutOfBoundsException: 2
+
+
+    }
+}
