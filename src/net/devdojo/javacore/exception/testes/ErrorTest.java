8ed04ce4d182e9a464d556ab3e7721442f03080d
José Malcher Junior <contato@josemalcher.net>
2017-04-07 17:46:37 -0300

    Aula 67: Exceptions pt 01

---
+++ src/net/devdojo/javacore/exception/testes/ErrorTest.java
@@ -0,0 +1,20 @@
+package net.devdojo.javacore.exception.testes;
+
+/**
+ * Criado por José Stélio Malcher Jr. on 07/04/2017.
+ *
+ *
+ * Curso Java Completo - Aula 67: Exceptions pt 01
+ *https://www.youtube.com/watch?v=MO12vFGPbxo&index=68&list=PL62G310vn6nHrMr1tFLNOYP_c73m6nAzL
+ *
+ */
+public class ErrorTest {
+    public static void main(String[] args) {
+        starckOverflowError();
+    }
+
+    public static void starckOverflowError(){
+        starckOverflowError();
+    }
+
+}
