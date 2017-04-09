5bc2358a4813a31e47ff32389693163a121fe425
José Malcher Junior <contato@josemalcher.net>
2017-04-09 08:33:50 -0300

    Aula 72: Tratamento de multiplas exceções pt 06

---
+++ src/net/devdojo/javacore/exception/runtimeexception/MultiplasRuntimeExceptionTest.java
@@ -0,0 +1,44 @@
+package net.devdojo.javacore.exception.runtimeexception;
+
+import java.io.FileNotFoundException;
+import java.sql.SQLDataException;
+
+/**
+ * Criado por José Stélio Malcher Jr. on 09/04/2017.
+ *
+ *Curso Java Completo - Aula 72: Tratamento de multiplas exceções pt 06
+ * https://www.youtube.com/watch?v=IL0-EWlytzA&list=PL62G310vn6nHrMr1tFLNOYP_c73m6nAzL&index=73
+ *
+ *
+ */
+public class MultiplasRuntimeExceptionTest {
+    public static void main(String[] args) {
+
+
+        try{
+            throw new RuntimeException();
+        }catch (ArrayIndexOutOfBoundsException e ){
+            System.out.println("Dentro de ArrayIndexOutOfBoundsException");
+        }catch (IllegalArgumentException e ){
+            System.out.println("Dentro de IllegalArgumentException");
+        }catch (ArithmeticException e ){
+            System.out.println("Dentro de ArithmeticException");
+        }catch (RuntimeException e ){ // mais generico - sempre por ultimo
+            System.out.println("Dentro de RuntimeException");
+        }
+        System.out.println("FIal do progama");
+
+        try {
+            telvezLanceException();
+        } catch (SQLDataException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void telvezLanceException() throws SQLDataException, FileNotFoundException{
+
+    }
+}
