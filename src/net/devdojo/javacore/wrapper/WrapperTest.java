2b65bcdc3522f1c28d68dfcb63947995fc418d61
José Malcher Junior <contato@josemalcher.net>
2017-04-10 15:23:07 -0300

    Aula 78: Classes Wrapper pt 01

---
+++ src/net/devdojo/javacore/wrapper/WrapperTest.java
@@ -0,0 +1,34 @@
+package net.devdojo.javacore.wrapper;
+
+/**
+ * Criado por José Stélio Malcher Jr. on 10/04/2017.
+ *
+ * Curso Java Completo - Aula 78: Classes Wrapper pt 01
+ * https://www.youtube.com/watch?v=MBm7iyYt6NQ&list=PL62G310vn6nHrMr1tFLNOYP_c73m6nAzL&index=79
+ *
+ *
+ */
+public class WrapperTest {
+    public static void main(String[] args) {
+        // 8 tipos primitivos
+        byte bytePrimitivo = 1;
+        short shotPrimitivo = 1;
+        int intPrimitivo = 10;
+        long longPrimitivo  = 10;
+        float floatPrimitivo = 10;
+        double doublePrimitivo = 10;
+        char charPrimitivo = 'A';
+        boolean booblean = true;
+
+        //Wrapper's
+        Byte byteWrapper = 1;
+        Short shortWrapper = 1;
+        Integer integerWrapper = 1;
+        Long longWrapper = 10L;
+        Float floatWrapper = 10F;
+        Double doublewrapper = 10D;
+        Character characterWrapper = 'A';
+        Boolean boobleanWrapper = true;
+
+    }
+}
